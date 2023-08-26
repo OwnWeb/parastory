10a396a0f8af13968ead5cf427f2ce4fa54f2ee1
Hugo Heneault <hugo@ownweb.fr>
2023-08-26 09:25:25 +0200

    👕(lint): Fix linter

--- database/factories/StoryFactory.php
+++ database/factories/StoryFactory.php
@@ -2,10 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Models\Author;
+use App\Models\Story;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Story>
+ * @extends Factory<Story>
  */
 class StoryFactory extends Factory
 {
@@ -23,7 +25,7 @@ class StoryFactory extends Factory
             'date' => $this->faker->date,
             'source_url' => $this->faker->url,
             'track_url' => $this->faker->url,
-            'author_id' => \App\Models\Author::factory(),
+            'author_id' => Author::factory(),
             'default_locale' => 'fr',
             'images' => [$this->faker->imageUrl],
         ];
